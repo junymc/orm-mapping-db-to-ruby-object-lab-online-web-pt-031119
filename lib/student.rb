0d0360b94d6a5eb5ea4418e1092afedf0af81253
Junyoung McArdle <juny.mcardle@gmail.com>
2019-05-26 00:57:49 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -72,7 +72,7 @@ class Student
     DB[:conn].execute(sql)
   end
 
-  def self.first_X_students_in_grade_10
+  def self.first_X_students_in_grade_10(x)
     sql = <<-SQL
     SELECT * FROM students
     WHERE grade = 10
