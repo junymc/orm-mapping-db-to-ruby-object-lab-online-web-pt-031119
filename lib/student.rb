00dc3d48a85ab86495d8eb90c17315235266aa3f
Junyoung McArdle <juny.mcardle@gmail.com>
2019-05-26 01:24:58 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -89,7 +89,8 @@ class Student
     WHERE grade = 10
     LIMIT 1
     SQL
-    DB[:conn].execute(sql)
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
   end
 
   def self.all_students_in_grade_X(x)
