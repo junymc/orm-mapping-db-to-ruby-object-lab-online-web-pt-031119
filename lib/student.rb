f912bc97c94f09996f90eece0ba61c7e04fc85ce
Junyoung McArdle <juny.mcardle@gmail.com>
2019-05-26 00:39:50 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,11 +11,11 @@ class Student
 
   def self.all
     sql = <<-SQL
-      SELECT * FROM students
+    SELECT *
+    FROM students
     SQL
-    DB[:conn].execute(sql, name).collect do |row|
+    DB[:conn].execute(sql).collect do |row|
       self.new_from_db(row)
-    end
   end
 
   def self.find_by_name(name)
